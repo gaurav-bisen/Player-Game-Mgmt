24b3cddd5b5f2d0852de89644b076f038779416f
gauravbisen1 <gouravbisen123@gmail.com>
2026-02-10 15:32:44 +0530

    middleware

--- middlewares/context.middleware.js
+++ middlewares/context.middleware.js
@@ -2,6 +2,7 @@ import db from '../models/index.js'
 
 export default function contextMiddleware(transactionStatus = false){
     return async (req, res, next)=> {
+
         //routes that dont need transaction
         if(!transactionStatus){
             req.context = { transaction: null};
@@ -21,9 +22,13 @@ export default function contextMiddleware(transactionStatus = false){
         //attach context to request
         req.context = {transaction};
 
+        //prevent double commit/rollback
         let isTransactionCompleted = false;
 
+        //function to commit or not
         const confirmTransaction = async (statusCode) => {
+
+            //prevents double execution
             if(isTransactionCompleted){
                 return;
             }
@@ -33,7 +38,7 @@ export default function contextMiddleware(transactionStatus = false){
             try {
                 if (statusCode >= 400) {
                     console.log("ROLLBACK....");
-                    await transaction.rollBack();
+                    await transaction.rollback();
                 } else {
                     console.log("COMMIT....");
                     await transaction.commit();
@@ -51,10 +56,13 @@ export default function contextMiddleware(transactionStatus = false){
         //request aborted
         res.on("close", ()=>{
             if(!res.writableEnded){
-                confirmTransaction(500);
+                void confirmTransaction(500); //Event listeners do not wait for promises.
             }
         });
 
         next();
     }
 }
+
+
+//why void -  Fire-and-forget: run transaction commit/rollback in background and intentionally ignore the returned Promise
